1a2baad18049a868343edcae481cae7df3e6c819
Fervea <kaios@hotmail.fr>
2010-10-26 15:56:57 +0200

    Fix compile, going to implement it later

--- src/server/game/AI/ScriptedAI/ScriptedEscortAI.cpp
+++ src/server/game/AI/ScriptedAI/ScriptedEscortAI.cpp
@@ -56,8 +56,8 @@ bool npc_escortAI::AssistPlayerInCombat(Unit* pWho)
         return false;
 
     //experimental (unknown) flag not present
-    /*if (!(me->GetCreatureInfo()->type_flags & CREATURE_TYPEFLAGS_UNK13))*/
-	if (!(me->GetCreatureInfo()->type_flags & CREATURE_TYPEFLAGS_AID_PLAYERS)
+    if (!(me->GetCreatureInfo()->type_flags & CREATURE_TYPEFLAGS_UNK13))
+	/*if (!(me->GetCreatureInfo()->type_flags & CREATURE_TYPEFLAGS_AID_PLAYERS)*/
         return false;
 
     //not a player
